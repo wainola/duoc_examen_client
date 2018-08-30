291d4c1831f714f1f2383cfa80f2ccd4e886a439
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-08-30 20:05:06 -0300

    setup for entry point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import Api from './api'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-css/semantic.min.css'
+
+import App from './routes'
+
+const middlewares = [ reduxThunk, logger ]
+const enhancers = applyMiddleware(...middlewares)
+const store = createStore(rootReducer, composeWithDevTools(enhancers))
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <Route component={App} />
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
