5dc4c98b4cd906e8ae12b87a5ae200f6aff91fb6
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-09-11 23:12:16 -0300

    working on the client

--- src/containers/UserDashboard/Components/Dashboard.js
+++ src/containers/UserDashboard/Components/Dashboard.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react'
 import DashboardView from '../Views/Dashboard.view'
 
 export class Dashboard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user: {
+        nombre:'',
+        apellido_paterno:'',
+        apellido_materno:'',
+        rut:''
+      }
+    }
+  }
+  componentWillMount(){
+    if(localStorage.user){
+      const user = JSON.parse(localStorage.getItem('user'))
+      this.setState({
+        user:{
+          ...user
+        }
+      })
+    }
+  }
   render() {
+    // console.log(JSON.parse(localStorage.getItem('user')))
     return (
       <div>
-        <DashboardView />
+        <DashboardView user={this.state.user}/>
       </div>
     )
   }
--- src/containers/UserDashboard/Views/Dashboard.view.js
+++ src/containers/UserDashboard/Views/Dashboard.view.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Responsive, Container, Grid, Segment, Card, Image, Icon } from 'semantic-ui-react'
 
 const DashboardView = props => {
   return (
-    <div>
-      <h1>DashBoard view!</h1>
-    </div>
+    <Responsive>
+      <Container>
+        <Grid>
+          <Grid.Row columns='2' stackable textAlign='center'>
+            <Grid.Column>
+              <Card >
+                <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+                <Card.Content>
+                  <Card.Header>Matthew</Card.Header>
+                  <Card.Meta>
+                    <span className='date'>Joined in 2015</span>
+                  </Card.Meta>
+                  <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    22 Friends
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              Acciones
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Responsive>
   )
 }
 
