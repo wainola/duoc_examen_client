b0e2df22bf08fd796fc30b5ee4fe65e6984a5fc2
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2018-09-13 13:31:35 -0300

    components on search

--- src/containers/GetCreditRequest/Components/SearchRequest.js
+++ src/containers/GetCreditRequest/Components/SearchRequest.js
@@ -8,15 +8,52 @@ import { bindActionCreators } from 'redux'
 export class SearchRequest extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      rut: '',
+      startDate: '',
+      endDate: ''
+    }
   }
   componentWillMount(){
     this.props.getCreditData()
+  }
+  onChange = e => {
+    e.preventDefault()
+    this.setState({
+      rut: e.target.value
+    })
+  }
+  handleStartDate = e => {
+    e.preventDefault()
+    this.setState({
+      startDate: e.target.value
+    })
+  }
+  handleEndDate = e => {
+    e.preventDefault()
+    this.setState({
+      endDate: e.target.value
+    })
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    if(this.state.rut !== ''){
+
+    }
+    if(this.state.startDate !== '' && this.state.endDate !== ''){
+
+    }
   }
   render() {
     console.log('search props', this.props)
     return (
       <div>
-        <SearchRequestView />
+        <SearchRequestView 
+        onChange={this.onChange}
+        handleStartDate={this.handleStartDate}
+        handleEndDate={this.handleEndDate}
+        onSubmit={this.onSubmit}
+        />
       </div>
     )
   }
--- src/containers/GetCreditRequest/Views/SearchRequest.view.js
+++ src/containers/GetCreditRequest/Views/SearchRequest.view.js
@@ -1,11 +1,53 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { Responsive, Container, Grid, Segment, Header, Form, Button } from 'semantic-ui-react'
 
 const SearchRequestView = props => {
   return (
     <div>
-      <h2>Search Request View</h2>
+      <Responsive>
+        <Container>
+          <Grid stackable>
+            <Grid.Row columns='2'>
+              <Grid.Column>
+                <Segment raised textAlign='left'>
+                  <Header as='h3'>Búsqueda por rut</Header>
+                  <Form onSubmit={props.onSubmit}>
+                    <Form.Field>
+                      <Form.Input type='text' placeholder='11.111.111-1' label='Ingrese rut' onChange={props.onChange} name='rut'/>
+                    </Form.Field>
+                    <Form.Field>
+                      <Button color='facebook'>Buscar</Button>
+                    </Form.Field>
+                  </Form>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment raised textAlign='left'>
+                  <Header as='h3'>Búsqueda por fecha</Header>
+                  <Form>
+                    <Form.Field>
+                      <Form.Input type='date' label='Fecha de Inicio' onChange={props.handleStartDate} />
+                    </Form.Field>
+                    <Form.Field>
+                      <Form.Input type='date' label='Fecha de Término'  onChange={props.handleEndDate}/>
+                    </Form.Field>
+                    <Form.Field>
+                      <Button color='facebook'>Buscar</Button>
+                    </Form.Field>
+                  </Form>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns='1'>
+              <Grid.Column>
+                <Segment raised>
+                  <Header as='h3'>Resultado</Header>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Responsive>
     </div>
   )
 }
